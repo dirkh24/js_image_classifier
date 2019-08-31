42285d9b88a3c94583ac0019a80353f932bee7f6
dirkh <githubdirk24@gmx.net>
2019-08-31 20:25:19 +0200

    function predict and handleFiles added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 let net;
 
-async function app() {
-  console.log('Loading mobilenet..');
+function handleFiles(files) {
+  console.log('Handling Files..');
+  $('.image-section').show();
+  $('#btn-predict').show();
+  // Load the model
+  
+} 
 
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
-
+async function predict() {
   // Make a prediction through the model on our image.
   const imgEl = document.getElementById('img');
   const result = await net.classify(imgEl);
@@ -14,6 +16,28 @@ async function app() {
   $('.loader').hide();
   $('#result').fadeIn(600);
   $('#result').text(result[0].className + ' with ' + (result[0].probability*100).toFixed(2) + '% probability.');
+}
+
+
+
+async function app() {
+  //document.getElementById("btn-fileSelect").onclick = handleFiles;
+  document.getElementById("btn-predict").onclick = predict;
+
+  const fileSelect = document.getElementById("btn-fileSelect"),
+  fileElem = document.getElementById("fileElem");
+
+  fileSelect.addEventListener("click", function (e) {
+    if (fileElem) {
+      fileElem.click();
+    }
+  }, false);
+
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
 }
 
 app();
